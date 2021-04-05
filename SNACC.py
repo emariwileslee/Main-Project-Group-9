34664092f89456381c104a25a53f1ea2a2797606
emariwileslee <ewileslee2016@fau.edu>
2021-04-05 09:53:49 -0400

    Started main SNACC program which will unify all the modules

---
+++ SNACC.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 09:37:32 2021
+
+@author: emari
+"""
+import webbrowser
+import os
+import sys
+sys.path.append(".\SNACC_Mapper\.")
+#from instascrape_ED_Multithreaded import InstaScrape
+#Other code and steps go here
+sys.path.append(".\SNACC_Visual\.")
+os.startfile('.\SNACC_Visual\demo.html')#Starts the visualizer menu
+
