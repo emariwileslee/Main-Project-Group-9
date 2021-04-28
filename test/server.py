3820f4cdb3b4fae323be10c7bbeb3737e174c43f
leucha <leucha@gmail.com>
2021-04-28 09:39:09 -0400

    added new data

--- test/server.py
+++ test/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 def hello():  
     data = []
-    with open('output1.csv', newline = '') as csvfile:
+    with open('Partial_Run.csv', newline = '') as csvfile:
         rows = csv.reader(csvfile, delimiter=",")
         i = 0
         for row in rows:
