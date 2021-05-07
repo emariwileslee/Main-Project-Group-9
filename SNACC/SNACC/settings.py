1200b1099c58de7b3e125576866012b696c9c416
leucha <leucha@gmail.com>
2021-05-07 15:33:52 -0400

    messing with django

--- SNACC/SNACC/settings.py
+++ SNACC/SNACC/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0rujfp(fi60yq=&l)-&$p@nww!ty(6yeb+sq@p9108@$@n1f(8'
+SECRET_KEY = 'django-insecure-7qn(klt-*blz!s#oc!t4#*0r)t!f(lkik250dq7n)5hn5mo(nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
