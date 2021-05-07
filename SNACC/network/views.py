9af4b0e19613280cfc2ecc7d36b0d9c9e39be680
leucha <leucha@gmail.com>
2021-05-07 16:06:23 -0400

    data is now flowing from csv on django

--- SNACC/network/views.py
+++ SNACC/network/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+from django.conf import settings
+import csv
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello bud.")
+    context = {}
+    data = []
+    with open(os.path.join(settings.BASE_DIR, 'Partial_Run.csv'), newline = '') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        i = 0 
+        for row in rows: 
+            if i != 0:
+                data.append(row)
+            i += 1
+            print(row)
+        context["data"] = data
+        return render(request, "network/index.html", context)
