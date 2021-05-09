6c79ed627aff21b931b4142359a2945029812a29
leucha <leucha@gmail.com>
2021-05-09 17:50:29 -0400

    Setup models on postgresql. Just gotta add data

--- SNACC/network/models.py
+++ SNACC/network/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100, blank=True, null = True)
+    bio = models.CharField(max_length=300, blank=True, null = True)
+    average_likes = models.IntegerField(blank=True, null=True)
+    num_followers = models.IntegerField()
+    num_following = models.IntegerField()
+
+class Connection(models.Model):
+    to = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='to_connections')
+    fro = models.ForeignKey(Account, on_delete=models.CASCADE, related_name = 'fro_connections')
+    connection_type = models.CharField(max_length=20)
